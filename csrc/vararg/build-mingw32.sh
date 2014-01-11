3737df087449edbcf4477237766ad7ff98b60ac5
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2014-01-11 13:13:01 +0200

    moved dll to clib

--- csrc/vararg/build-mingw32.sh
+++ csrc/vararg/build-mingw32.sh
@@ -1,3 +1,3 @@
-gcc vararg.c -O3 -s -o ../../bin/mingw32/vararg.dll -shared -ansi -I../lua -L../../bin/mingw32 -llua51
+gcc vararg.c -O3 -s -o ../../bin/mingw32/clib/vararg.dll -shared -ansi -I../lua -L../../bin/mingw32 -llua51
 
 #../../bin/luajit vararg_test.lua
