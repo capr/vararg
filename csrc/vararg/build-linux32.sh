c56fef6ca174f813ae4f255f26c4fcc67daf74d9
Cosmin Apreuetsei <cosmin.apreutesei@gmail.com>
2014-01-23 00:53:10 +0200

    removed CFLAGS/CXXFLAGS

--- csrc/vararg/build-linux32.sh
+++ csrc/vararg/build-linux32.sh
@@ -1,3 +1,3 @@
-gcc $CFLAGS vararg.c -shared -o ../../bin/linux32/clib/vararg.so -ansi -I../lua
+gcc -O2 -s -static-libgcc vararg.c -shared -o ../../bin/linux32/clib/vararg.so -ansi -I../lua
 
 #../../linux/bin/luajit vararg_test.lua
--- csrc/vararg/build-mingw32.sh
+++ csrc/vararg/build-mingw32.sh
@@ -1,3 +1,3 @@
-gcc $CFLAGS vararg.c -shared -o ../../bin/mingw32/clib/vararg.dll -ansi -I../lua -L../../bin/mingw32 -llua51
+gcc -O2 -s -static-libgcc vararg.c -shared -o ../../bin/mingw32/clib/vararg.dll -ansi -I../lua -L../../bin/mingw32 -llua51
 
 #../../bin/luajit vararg_test.lua
