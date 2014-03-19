62d12666e6b8dd861bd6626a9d685b6808852d56
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2014-03-19 23:36:43 +0200

    osx32 build

---
+++ csrc/vararg/build-osx32.sh
@@ -0,0 +1,1 @@
+gcc -arch i386 -O2 vararg.c -shared -o ../../bin/osx32/clib/vararg.so -ansi -I../lua -undefined dynamic_lookup
--- csrc/vararg/build-osx64.sh
+++ csrc/vararg/build-osx64.sh
@@ -1,1 +1,1 @@
-gcc -O2 vararg.c -shared -o ../../bin/osx64/clib/vararg.so -ansi -I../lua -undefined dynamic_lookup
+gcc -arch x86_64 -O2 vararg.c -shared -o ../../bin/osx64/clib/vararg.so -ansi -I../lua -undefined dynamic_lookup
